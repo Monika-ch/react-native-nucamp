95fe71f2c3c9640a94b255eddbbe54e4fc7e6941
monika <monika12b@gmail.com>
2020-05-30 11:49:53 -0700

    disabling warnings in viewport

--- App.js
+++ App.js
@@ -2,6 +2,6 @@ import React from "react";
 import Main from "./components/MainComponent";
 
 export default function App() {
+  console.disableYellowBox = true;
   return <Main />;
-  // addigna  comment
 }
