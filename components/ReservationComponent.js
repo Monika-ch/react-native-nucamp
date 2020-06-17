998776138d9026b2b5f2f0a30d77448af79c975b
monika <monika12b@gmail.com>
2020-06-17 00:02:38 -0700

    Local Notification

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -10,6 +10,8 @@ import {
 } from "react-native";
 import DatePicker from "react-native-datepicker";
 import * as Animatable from "react-native-animatable";
+import * as Permissions from "expo-permissions";
+import { Notifications } from "expo";
 
 class Reservation extends Component {
   constructor(props) {
@@ -43,6 +45,32 @@ class Reservation extends Component {
       date: "",
       showModal: false,
     });
+  }
+
+  async obtainNotificationPermission() {
+    const permission = await Permissions.getAsync(
+      Permissions.USER_FACING_NOTIFICATIONS
+    );
+    if (permission.status !== "granted") {
+      const permission = await Permissions.askAsync(
+        Permissions.USER_FACING_NOTIFICATIONS
+      );
+      if (permission.status !== "granted") {
+        Alert.alert("Permission not granted to show notifications");
+      }
+      return permission;
+    }
+    return permission;
+  }
+
+  async presentLocalNotification(date) {
+    const permission = await this.obtainNotificationPermission();
+    if (permission.status === "granted") {
+      Notifications.presentLocalNotificationAsync({
+        title: "Your Campsite Reservation Search",
+        body: "Search for" + date + "requested",
+      });
+    }
   }
 
   render() {
@@ -119,7 +147,6 @@ class Reservation extends Component {
                   {
                     text: "Cancel",
                     onPress: () => {
-                      this.toggleModal();
                       this.resetForm();
                     },
                     style: " cancel",
@@ -127,7 +154,7 @@ class Reservation extends Component {
                   {
                     text: "OK",
                     onPress: () => {
-                      this.toggleModal();
+                      this.presentLocalNotification(this.state.date);
                       this.resetForm();
                     },
                   },
